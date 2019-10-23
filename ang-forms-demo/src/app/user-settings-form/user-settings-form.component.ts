58795ab58681d2bcfc4e3db6efc9c26ef6d20777
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-23 21:21:44 +0300

    Created two way data binding etc

--- ang-forms-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ang-forms-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormComponent implements OnInit {
 
+  userSettings : UserSettings = {
+    name : "Ted",
+    emailOffers : true,
+    interfaceStyle : "dark",
+    subscriptionType : "Annually",
+    notes : "Here are some notes"
+  };
   constructor() { }
 
   ngOnInit() {
