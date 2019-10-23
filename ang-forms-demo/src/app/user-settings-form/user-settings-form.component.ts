0f011cc1c38940eacb2b1b2480f92819f0dab0c8
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-23 23:06:06 +0300

    Added validation to form

--- ang-forms-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ang-forms-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -9,15 +10,22 @@ import { UserSettings } from '../data/user-settings';
 export class UserSettingsFormComponent implements OnInit {
 
   userSettings : UserSettings = {
-    name : "Ted",
-    emailOffers : true,
-    interfaceStyle : "dark",
-    subscriptionType : "Annually",
-    notes : "Here are some notes"
+    name : null,
+    emailOffers : null,
+    interfaceStyle : null,
+    subscriptionType : null,
+    notes : null
   };
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onBlur(field : NgModel){
+    console.log("in onBlur", field.valid);
+  }
+  onSubmit(form : NgForm){
+    console.log("in onSubmit " , form.valid);
   }
 
 }
